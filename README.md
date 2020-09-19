9b0819cea495958bdd20bade25a68d071287f0b0
stefan dili <stefan.dili.021@gmail.com>
2020-09-19 23:57:35 +0200

    initialized readme, and working on build

---
+++ README.md
@@ -0,0 +1,49 @@
+# Hack Attack!
+
+This Project represents my attempt at a simple web game made with [Phaser.js](https://phaser.io/), A JavaScript framework for building games that utilizes the HTML canvas element.
+
+## Built With
+
+- Javascript
+- Phaser
+- Webpack
+- OpenGameArt assets
+- Icons8 icons
+- Jest
+
+## Live Demo
+
+- [Live Demo Link]
+-
+## Setup
+
+To get a local copy up and running follow these simple example steps:
+
+1. Clone this repo to your computer using <code>git clone</code>
+2. Open index.html file on the local Live server to run the app.
+
+## Author
+
+👤 **Stefan Dili**
+
+- Github: [Stefan Dili](https://github.com/dili021)
+- Twitter: [@dilistefan](https://twitter.com/dilistefan)
+- Linkedin: [Stefan Dili](https://www.linkedin.com/in/stefan-dili/)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
+
+Feel free to check the [issues page](https://github.com/dili021/ticTacToe/issues).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+Referred Odin project to learn about ES6
+
+## 📝 License
+
+This project is [MIT](lic.url) licensed.
--- src/js/scenes/ScenePreload.js
+++ src/js/scenes/ScenePreload.js
@@ -73,33 +73,33 @@ export default class PreloaderScene extends Phaser.Scene {
 
     this.timedEvent = this.time.delayedCall(3000, this.ready, [], this);
 
-    this.load.audio('scoreMusic', 'src/assets/audio/scoreMusic.mp3');
-    this.load.image('sprBg0', 'src/assets/Matrix_large.png');
-    this.load.image('sprBtnPlay', 'src/assets/sprBtnPlay.png');
-    this.load.image('sprBtnAudio', 'src/assets/sprBtnAudio.png');
-    this.load.image('sprBtnPlayHover', 'src/assets/sprBtnPlayHover.png');
-    this.load.image('sprBtnPlayDown', 'src/assets/sprBtnPlayDown.png');
-    this.load.image('sprBtnRestart', 'src/assets/sprBtnRestart.png');
-    this.load.image('sprBtnRestartHover', 'src/assets/sprBtnRestartHover.png');
-    this.load.image('sprBtnRestartDown', 'src/assets/sprBtnRestartDown.png');
-    this.load.html('form', 'src/assets/input/form.html');
-    this.load.audio('sndBtnOver', 'src/assets/audio/sndBtnOver.wav');
-    this.load.audio('sndBtnDown', 'src/assets/audio/sndBtnDown.wav');
-    this.load.audio('menuMusic', 'src/assets/audio/menuMusic.mp3');
-    this.load.image('background', 'src/assets/Matrix_large.png');
-    this.load.spritesheet('sprExplosion', 'src/assets/sprExplosion.png', {
+    this.load.audio('scoreMusic', 'assets/audio/scoreMusic.mp3');
+    this.load.image('sprBg0', 'assets/Matrix_large.png');
+    this.load.image('sprBtnPlay', 'assets/sprBtnPlay.png');
+    this.load.image('sprBtnAudio', 'assets/sprBtnAudio.png');
+    this.load.image('sprBtnPlayHover', 'assets/sprBtnPlayHover.png');
+    this.load.image('sprBtnPlayDown', 'assets/sprBtnPlayDown.png');
+    this.load.image('sprBtnRestart', 'assets/sprBtnRestart.png');
+    this.load.image('sprBtnRestartHover', 'assets/sprBtnRestartHover.png');
+    this.load.image('sprBtnRestartDown', 'assets/sprBtnRestartDown.png');
+    this.load.html('form', 'assets/input/form.html');
+    this.load.audio('sndBtnOver', 'assets/audio/sndBtnOver.wav');
+    this.load.audio('sndBtnDown', 'assets/audio/sndBtnDown.wav');
+    this.load.audio('menuMusic', 'assets/audio/menuMusic.mp3');
+    this.load.image('background', 'assets/Matrix_large.png');
+    this.load.spritesheet('sprExplosion', 'assets/sprExplosion.png', {
       frameWidth: 32,
       frameHeight: 32,
     });
-    this.load.image('sprEnemy0', 'src/assets/bug.png');
+    this.load.image('sprEnemy0', 'assets/bug.png');
 
-    this.load.image('sprLaserEnemy0', 'src/assets/sprLaserEnemy0.png');
-    this.load.image('sprLaserPlayer', 'src/assets/player_laser.png');
-    this.load.image('sprPlayer', 'src/assets/player_avatar.png');
-    this.load.audio('sndExplode0', 'src/assets/audio/sndExplode0.wav');
-    this.load.audio('sndExplode1', 'src/assets/audio/sndExplode1.wav');
-    this.load.audio('sndLaser', 'src/assets/audio/sndLaser.wav');
-    this.load.audio('backgroundMusic', 'src/assets/audio/backgroundMusic.mp3');
+    this.load.image('sprLaserEnemy0', 'assets/sprLaserEnemy0.png');
+    this.load.image('sprLaserPlayer', 'assets/player_laser.png');
+    this.load.image('sprPlayer', 'assets/player_avatar.png');
+    this.load.audio('sndExplode0', 'assets/audio/sndExplode0.wav');
+    this.load.audio('sndExplode1', 'assets/audio/sndExplode1.wav');
+    this.load.audio('sndLaser', 'assets/audio/sndLaser.wav');
+    this.load.audio('backgroundMusic', 'assets/audio/backgroundMusic.mp3');
   }
 
   ready() {
